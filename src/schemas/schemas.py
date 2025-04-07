0deb636fd8f319cefff6a1d642cb605575d13ab7
Kim Aleksandr <ksairos.dev@gmai.com>
2025-04-07 19:49:57 +0900

    updated GrammarEntry class

--- src/schemas/schemas.py
+++ src/schemas/schemas.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass
-from typing import Literal, Optional
+from typing import Literal, Optional, List, Dict
 
 from fastembed import SparseTextEmbedding
 from openai import AsyncOpenAI
@@ -17,10 +17,23 @@ class RetrieverDeps:
 class ChatMessage(BaseModel):
     content: str
 
+
+class GrammarEntry(BaseModel):
+    level: Literal[1, 2, 3, 4, 5, 6]
+    grammar_name_kr: str
+    grammar_name_rus: str
+    description: str
+    usage_form: str
+    examples: List[Dict[str, str]]
+    notes: Optional[List[str]]
+    # TODO: Add irregular verbs examples
+    # with_irregular_verbs: Optional[List[str]]
+
+
+
 class RetrievedDocs(BaseModel):
-    content: str
-    metadata: dict
+    content: GrammarEntry
     score: float
     cross_score: Optional[float] = None
-
-
+    
+    
