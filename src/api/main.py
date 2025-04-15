78f3fa1e4d74aecbf0fad4c0be920966aa827c48
Kim Aleksandr <ksairos.dev@gmai.com>
2025-04-15 15:51:31 +0900

    added model caching for faster build

--- src/api/main.py
+++ src/api/main.py
@@ -9,10 +9,11 @@ from openai import AsyncOpenAI
 from pydantic import BaseModel
 from pydantic_ai.usage import UsageLimits
 from qdrant_client import AsyncQdrantClient, QdrantClient
-from sqlalchemy.orm import Session
+from sqlalchemy.ext.asyncio import AsyncSession
 
 from src.config.settings import Config
 from src.db.database import get_db
+from src.db.models import ChatModel
 from src.llm_agent.agent import router_agent
 from src.schemas.schemas import RouterAgentDeps, RouterAgentResult
 
@@ -27,8 +28,10 @@ qdrant_client = QdrantClient(
     host=config.qdrant_host_docker,
     port=config.qdrant_port,
 )
-sparse_embedding = SparseTextEmbedding(config.sparse_embedding_model)
-reranking_model = CrossEncoder(config.reranking_model)
+
+# Set up in compose using model_cache volume
+sparse_embedding = SparseTextEmbedding(config.sparse_embedding_model, cache_dir="/root/.cache/huggingface/hub")
+reranking_model = CrossEncoder(config.reranking_model, cache_folder="/root/.cache/huggingface/hub")
 
 
 logfire.configure(token=config.logfire_api_key)
@@ -37,6 +40,7 @@ logfire.instrument_fastapi(app)
 
 class Message(BaseModel):
     user_prompt: str
+    chat_id: int
 
 @app.get("/")
 async def root():
@@ -44,7 +48,7 @@ async def root():
 
 
 @app.post("/invoke")
-async def process_message(message: Message, db: Session = Depends(get_db)):
+async def process_message(message: Message, db: AsyncSession = Depends(get_db)):
 
     logfire.info(f'User message "{message}"')
     
@@ -56,13 +60,21 @@ async def process_message(message: Message, db: Session = Depends(get_db)):
         reranking_model=reranking_model,
         db=db
     )
-    
+
+    r = await db.get(ChatModel, message.chat_id)
+    message_history = r.message_history
+
     response = await router_agent.run(
         user_prompt=message.user_prompt,
         deps=deps,
         usage_limits=UsageLimits(request_limit=5),
-        result_type=RouterAgentResult
+        result_type=RouterAgentResult,
+        message_history=message_history,
     )
+
+    logfire.info(f"All messages: {response.all_messages_json()}")
+
+    new_messages = response.new_messages_json()
 
     # response = await grammar_agent.run(message.user_prompt, deps=deps)
     logfire.info("Response: {response}", response=response.data)
