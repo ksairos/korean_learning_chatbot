1fd11b0f256e70e9db2be29ab5c826da956ae5e9
ksairos <ksairos.dev@gmail.com>
2025-09-06 15:42:38 +0900

    rem comment

--- src/tgbot/bot.py
+++ src/tgbot/bot.py
@@ -60,7 +60,7 @@ def setup_logging():
     Set up logging configuration for the application.
 
     This method initializes the logging configuration for the application.
-    It sets the log level to INFO and configures a basic colorized log for
+    It sets the log level to "info" and configures a basic colorized log for
     output. The log format includes the filename, line number, log level,
     timestamp, logger name, and log message.
 
