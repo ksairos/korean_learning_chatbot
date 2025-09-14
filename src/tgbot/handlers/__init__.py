c0c6be3765b08848d8ec92cfd94cdf6c260604ae
ksairos <ksairos.dev@gmail.com>
2025-09-14 17:12:24 +0900

    fixes

--- src/tgbot/handlers/__init__.py
+++ src/tgbot/handlers/__init__.py
@@ -3,13 +3,11 @@ from .admin import admin_router
 from .chat import chat_router
 from .user import user_router
 from .help import help_router
-from .vocab import dictionary_router
 
 routers_list = [
     admin_router,
     user_router,
     help_router,
-    dictionary_router,
     chat_router,  # must be last
 ]
 
