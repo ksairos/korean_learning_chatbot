3999524df617073e4b0bcdcbd9a2af4cb23e8a22
ksairos <ksairos.dev@gmail.com>
2025-09-20 16:06:45 +0900

    more prompting fixes

--- src/llm_agent/agent_tools.py
+++ src/llm_agent/agent_tools.py
@@ -61,26 +61,29 @@ async def retrieve_grammars_tool(
         score_threshold=vector_threshold,
     )
 
-    # Use hybrid search with bm25 amd OpenAI embeddings with RRF
-    response = await deps.qdrant_client.query_points(
-        collection_name=config.qdrant_collection_name_final,
-        prefetch=[bm_25_prefetch, dense_prefetch],
-        query=FusionQuery(fusion=Fusion.RRF),
-        with_payload=True,
-    )
-    hits = response.points
-
-    logfire.info(f"Received {len(hits)} results from Qdrant.")
-
-    # Convert to schema objects
-    docs = [
-        RetrievedGrammar(
-            id=hit.id,
-            content=GrammarEntryV2(**hit.payload),
-            score=hit.score,
+    with logfire.span(f"Querying Qdrant for search_query = {search_query}"):
+        # Use hybrid search with bm25 amd OpenAI embeddings with RRF
+        response = await deps.qdrant_client.query_points(
+            collection_name=config.qdrant_collection_name_final,
+            prefetch=[bm_25_prefetch, dense_prefetch],
+            query=FusionQuery(fusion=Fusion.RRF),
+            with_payload=True,
         )
-        for hit in hits
-    ]
+        hits = response.points
+
+        logfire.info(f"Received {len(hits)} results from Qdrant.")
+
+        # Convert to schema objects
+        docs = [
+            RetrievedGrammar(
+                id=hit.id,
+                content=GrammarEntryV2(**hit.payload),
+                score=hit.score,
+            )
+            for hit in hits
+        ]
+
+        logfire.info(f"Retrieved docs: {docs}")
 
     if not docs:
         logfire.info("No documents found.")
@@ -103,10 +106,53 @@ async def retrieve_grammars_tool(
             instrument=True,
             output_type=List[int],
             instructions="""
-                You're a search filter in Korean grammar database. Select all relevant search results from the 
-                GRAMMAR LIST, based on the USER QUERY, and output their index only in a list. Focus on higher recall, 
-                rather than precision, but if none are relevant, output an empty list
-            """
+You're a search filter in Korean grammar database. Select all relevant search results from the GRAMMAR LIST, 
+based on the USER QUERY, and output their index only in a list. Focus on higher recall, if the number of potential 
+results is more that 1, and higher precision if there is only 1 relevant result (i.e. it should be exactly what the 
+user is looking for. If none are relevant, output an empty list
+
+Example 1 - high recall:
+```
+USER_QUERY: 'грамматика будущего времени в корейском языке'
+GRAMMAR LIST: 
+0. V/A + -(으)ㄹ 것이다 - будущее время
+1. V + -겠- - будущее время (планы, намерения говорящего)
+2. A/V + -(으)ㄹ 때, N + 때 - - «когда…», «во время…»
+3. N + 에 - «в (какое-то время)»
+4. V + -(으)ㄹ - определительная форма глагола в будущем времени
+5. V/A + -(으)면서 - одновременность действий
+6. V/A + -었-, -았-, -였- - суффикс прошедшего времени
+
+OUTPUT: [0, 1, 4]
+```
+
+Example 2 - higher precision
+```
+USER_QUERY: 'грамматика 는 데'
+GRAMMAR LIST:
+0. N + 은/는 - выделительная частица
+1. N + 하고 - «с» (совместное действие)
+2. V + -는 동안(에) - «в течение…, пока…»
+3. V/A + -(으)ㄴ/는데 - «а», «но», вводит контраст, предысторию или контекст
+4. V + -는 것 - «делание», «то, что…», отглагольное существительное
+
+
+OUTPUT: [3]
+```
+
+Example 3 - high precision, but none relevant:
+```
+USER_QUERY: 'объясни использование 아/어 보이다'
+GRAMMAR LIST:
+0. 보다 - «чем»
+1. V + -(으)ㄹ까 보다 - «боюсь, что…», «волнуюсь, что…»
+2. V + -아/어 있다 - состояние, возникшее в результате действия
+3. V + -고 있다 - состояние одежды и внешнего вида
+4. 와/과 - «с», совместное действие
+
+OUTPUT: []
+```
+"""
         )
 
         llm_filter_response = await llm_filter_agent.run(user_prompt="\n\n".join(llm_filter_prompt))
